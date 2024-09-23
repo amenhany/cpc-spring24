bd531cd5034c562dd7317859b5533085e620e4f4
Amen Hany <amen.hany.2810@gmail.com>
2024-09-23 15:39:19 +0300

    5/J I hate my life

--- 5/J.cpp
+++ 5/J.cpp
@@ -13,19 +13,15 @@ int main() {
         for (int i = 0; i < nOfMonsters; i++) std::cin >> monsters[i].second;
         for (int i = 0; i < nOfMonsters; i++) std::cin >> monsters[i].first;
         std::sort(monsters.begin(), monsters.end());
- 
-        while (genosPower > 0) {
-            int temp = genosPower;
-            bool done = true;
-            for (int i = 0; i < monsters.size(); i++) {
-                if (monsters[i].second > 0) {
-                    monsters[i].second -= temp;
-                    if (genosPower == temp) genosPower -= monsters[i].first;
-                    done = false;
-                } else continue;
+
+        int damage = genosPower;
+        for (int i = 0; i < nOfMonsters; i++) {
+            if (monsters[i].second > damage) {
+                genosPower -= monsters[i].first;
+                damage += genosPower;
+                i--;
             }
-            if (done) break;
-        }
+        }       
  
         if (genosPower > 0) std::cout << "YES\n";
         else std::cout << "NO\n";
