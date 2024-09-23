7b713ab59ac5f1a49592b882094c8e4f9fba22ef
Amen Hany <amen.hany.2810@gmail.com>
2024-09-23 14:48:38 +0300

    5/J still unoptimized

--- 5/J.cpp
+++ 5/J.cpp
@@ -4,31 +4,27 @@
  
 typedef long long ll;
  
-bool sortBySecond(std::pair<ll, ll> &p1, std::pair<ll, ll> &p2) {
-    if (p1.second > p2.second) return 1;
-    else return 0;
-}
- 
 int main() {
     int t, nOfMonsters, genosPower;
     std::cin >> t;
     while (t--) {
         std::cin >> nOfMonsters >> genosPower;
         std::vector<std::pair<ll, ll>> monsters(nOfMonsters);
-        for (int i = 0; i < nOfMonsters; i++) std::cin >> monsters[i].first;
         for (int i = 0; i < nOfMonsters; i++) std::cin >> monsters[i].second;
-        std::sort(monsters.begin(), monsters.end(), sortBySecond);
+        for (int i = 0; i < nOfMonsters; i++) std::cin >> monsters[i].first;
+        std::sort(monsters.begin(), monsters.end());
  
-        while (genosPower > 0 && monsters.size() > 0) {
-            for (int i = 0; i < monsters.size(); i++) monsters[i].first -= genosPower;
-            while (monsters.size() > 0) {
-                if (monsters.back().first > 0) {
-                    genosPower -= monsters.back().second;
-                    break;
-                }
-                else if (monsters.size() > 0) monsters.pop_back();
-                else break;
+        while (genosPower > 0) {
+            int temp = genosPower;
+            bool done = true;
+            for (int i = 0; i < monsters.size(); i++) {
+                if (monsters[i].second > 0) {
+                    monsters[i].second -= temp;
+                    if (genosPower == temp) genosPower -= monsters[i].first;
+                    done = false;
+                } else continue;
             }
+            if (done) break;
         }
  
         if (genosPower > 0) std::cout << "YES\n";
