9e80d1c0d770908bdc4bead2949fc1282da46253
Amen Hany <amen.hany.2810@gmail.com>
2024-09-24 22:01:26 +0300

    graduation/B 1017A The Rank (sometimes the simplest is the best)

--- graduation23/B.cpp
+++ graduation23/B.cpp
@@ -2,13 +2,6 @@
 #include <vector>
 #include <algorithm>
 
-
-bool compare(std::pair<int, int> &p1, std::pair<int, int> &p2) {
-    if (p1.first > p2.first) return 1;
-    else if (p1.second < p2.second) return 1;
-    else return 0;
-}
-
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL); std::cout.tie(NULL);
@@ -16,7 +9,7 @@ int main() {
     int n;
     std::cin >> n;
 
-    std::vector<std::pair<int, int>> ranking(n);
+    int ranking[n];
     for (int i = 0; i < n; i++) {
         int sum = 0;
         for (int i = 0; i < 4; i++) {
@@ -25,11 +18,15 @@ int main() {
             sum += input;
         }
 
-        ranking[i] = {sum, i+1};
+        ranking[i] = sum;
     }
 
-    sort(ranking.begin(), ranking.end(), compare);
+    int tom = ranking[0];
+    std::sort(ranking, ranking + n, std::greater<int>());
     for (int i = 0; i < n; i++) {
-        if (ranking[i].second == 1) std::cout << i+1 << '\n';
+        if (ranking[i] == tom) {
+            std::cout << i+1 << '\n';
+            break;
+        }
     }
 }
